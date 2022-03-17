8fd208779cf4ec11cb0c115638cb954af366c13c
Therese Grass <tg222kv@student.lnu.se>
2022-03-17 08:34:33 +0100

    adding code in sailboat class

--- app/src/main/java/assignment4/boatclub/Sailboat.java
+++ app/src/main/java/assignment4/boatclub/Sailboat.java
@@ -3,6 +3,32 @@ package assignment4.boatclub;
 /**
  * Represents a Sailboat class.
  */
-public class Sailboat {
+public class Sailboat extends Boat {
+  private int depth;
+
+  public Sailboat() {
+
+  }
+
+  public Sailboat(String name, int length, int depth) {
+    super(name, length);
+  }
+
+  public int getDepth() {
+    return depth;
+  }
+
+  /**
+   * Sets the depth of the sailboat.
+   *
+   * @param theDepth - the depth.
+   */
+  public void setDepth(int theDepth) {
+    if (theDepth > 0) {
+      this.depth = theDepth;
+    } else {
+      System.out.println("The depth of the boat must be deeper than zero meters.");
+    }
+  }
   
 }
