9bb4a6a73eefafa35bbcf9312d505676898b0fa7
Therese Grass <tg222kv@student.lnu.se>
2022-03-17 08:11:41 +0100

    adding code in member class

--- app/src/main/java/assignment4/boatclub/Motorboat.java
+++ app/src/main/java/assignment4/boatclub/Motorboat.java
@@ -3,6 +3,23 @@ package assignment4.boatclub;
 /**
  * Represents a Motorboat class.
  */
-public class Motorboat {
+public class Motorboat extends Boat {
+  private int horsepower;  
+
+  public Motorboat() {
+
+  }
+
+  public Motorboat(String name, int length, int horsepower) {
+    super(name, length);
+  }
+
+  public int getHorsepower() {
+    return horsepower;
+  }
+
+  public void setHorsepower(int horsepower) {
+    this.horsepower = horsepower;
+  }
   
 }
